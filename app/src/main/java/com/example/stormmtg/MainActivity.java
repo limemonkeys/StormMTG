6d019da72c4f3c01bcbfb3d05e0abeec41e8ad10
Joseph Burton <js562338@dal.ca>
2021-07-15 14:29:45 -0300

    Added onClicks. Updated action bar colour

--- app/src/main/java/com/example/stormmtg/MainActivity.java
+++ app/src/main/java/com/example/stormmtg/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.stormmtg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,104 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button increaseLifeButton = findViewById(R.id.lifeIncrease);
+        Button decreaseLifeButton = findViewById(R.id.lifeDecrease);
+        TextView lifeCounter = findViewById(R.id.lifeCounter);
+
+        Button increaseOpponentLifeButton = findViewById(R.id.opponentLifeIncrease);
+        Button decreaseOpponentLifeButton = findViewById(R.id.opponentLifeDecrease);
+        TextView opponentLifeCounter = findViewById(R.id.opponentLifeCounter);
+
+        Button increaseStormButton = findViewById(R.id.stormIncrease);
+        Button decreaseStormButton = findViewById(R.id.stormDecrease);
+        TextView stormCounter = findViewById(R.id.stormCounter);
+
+        Button increaseBlueButton = findViewById(R.id.blueIncrease);
+        Button decreaseBlueButton = findViewById(R.id.blueDecrease);
+        TextView blueCounter = findViewById(R.id.blueCounter);
+
+        Button increaseRedButton = findViewById(R.id.redIncrease);
+        Button decreaseRedButton = findViewById(R.id.redDecrease);
+        TextView redCounter = findViewById(R.id.redCounter);
+
+        increaseLifeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lifeCounter.setText(String.valueOf(Integer.parseInt((String) lifeCounter.getText()) + 1));
+            }
+        });
+
+        decreaseLifeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lifeCounter.setText(String.valueOf(Integer.parseInt((String) lifeCounter.getText()) - 1));
+            }
+        });
+
+        increaseOpponentLifeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opponentLifeCounter.setText(String.valueOf(Integer.parseInt((String) opponentLifeCounter.getText()) + 1));
+            }
+        });
+
+        decreaseOpponentLifeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opponentLifeCounter.setText(String.valueOf(Integer.parseInt((String) opponentLifeCounter.getText()) - 1));
+            }
+        });
+
+        increaseStormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stormCounter.setText(String.valueOf(Integer.parseInt((String) stormCounter.getText()) + 1));
+            }
+        });
+
+        decreaseStormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentStormValue = Integer.parseInt((String) stormCounter.getText());
+                if (currentStormValue != 0){
+                    stormCounter.setText(String.valueOf(currentStormValue - 1));
+                }
+            }
+        });
+
+        increaseBlueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blueCounter.setText(String.valueOf(Integer.parseInt((String) blueCounter.getText()) + 1));
+            }
+        });
+
+        decreaseBlueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentStormValue = Integer.parseInt((String) blueCounter.getText());
+                if (currentStormValue != 0){
+                    blueCounter.setText(String.valueOf(currentStormValue - 1));
+                }
+            }
+        });
+
+        increaseRedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redCounter.setText(String.valueOf(Integer.parseInt((String) redCounter.getText()) + 1));
+            }
+        });
+
+        decreaseRedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentStormValue = Integer.parseInt((String) redCounter.getText());
+                if (currentStormValue != 0){
+                    redCounter.setText(String.valueOf(currentStormValue - 1));
+                }
+            }
+        });
     }
 }
